f319815af0857accd4455f968ce3f9f0f7798654
Juicestus <jus@gtsbr.org>
2022-11-07 21:10:19 -0600

    F_TEST_CASE unformatted

--- src/test.c
+++ src/test.c
@@ -8,5 +8,10 @@ int main(const int argc, const char** argv)
 {
   // Example
   F_PRINT_CALL(m_normal_cdf_range, 2., 5., 2.5, 0.5);
+
+  F_TEST_CASE(6., m_test, 2., 3.);
+  F_TEST_CASE(7., m_test, 2., 3.);
+  F_TEST_CASE(ERROR, m_test, 2., 3.);
+
   return 0;
 }
--- src/test_macros.h
+++ src/test_macros.h
@@ -5,6 +5,7 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #define __VAR_FLOAT_TERMINATOR -.8028533846193f 
 #define __PRINT_FUNCTION_SIGNATURE(f) printf("%s(", #f)
@@ -42,7 +43,49 @@ static void __print_variatic_floats(char* str_delim, ...)
     if (!f(__VA_ARGS__, &__r_##f))                                             \
       printf(__FLOAT_FORMAT "\n", __r_##f);                                    \
     else                                                                       \
-      printf("Error\n");                                                       \
+      printf("ERROR\n");                                                       \
   }
+
+#define ERROR 1.
+
+/// Nice way to print functions.
+/// Params:
+///   e: Expected result or ERROR if expected to error
+///   f: Function that:
+///     - Takes any number of floats arguments
+///     - Last parameter is a float* result buffer
+///     - Returns 0 on success, 1 on failure
+///   ...: Any number of floats to pass to f
+#define F_TEST_CASE(e, f, ...)\
+  {\
+     float __r_##f;\
+     int wf = !strcmp("ERROR", #e);\
+    __PRINT_FUNCTION_SIGNATURE(f);                                             \
+    __PRINT_FUNCTION_ARGUMENTS(__VA_ARGS__);                                   \
+    __PRINT_FUNCTION_CLOSURE();      \
+    int pass;\
+    if (!f(__VA_ARGS__, &__r_##f)) {                                             \
+      printf(__FLOAT_FORMAT, __r_##f);                                    \
+      pass = __r_##f == e && !wf;\
+    } else {                                                                       \
+      printf("ERROR");                                                       \
+      pass = wf;\
+    }\
+    printf(" %s ", pass ? "==" : "!=");\
+    if (wf)\
+      printf("ERROR");\
+    else\
+      printf(__FLOAT_FORMAT, e);\
+    printf(" [%s]\n", pass ? "PASS" : "FAIL");\
+  }
+
+/// A goofy function to test F_TEST_CASE
+int m_test(const float v1, const float v2, float* result)
+{
+  if (v1 == v2)
+    return 1;
+  *result = v1 * v2;
+  return 0;
+}
 
 #endif
